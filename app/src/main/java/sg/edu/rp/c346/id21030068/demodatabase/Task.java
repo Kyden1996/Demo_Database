da76ab9c37fa5dd38ff4d04d33df1ef3ce4cc190
21030068 <21030068@myrp.edu.sg>
2022-07-25 17:36:57 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21030068/demodatabase/Task.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21030068.demodatabase;
+
+public class Task {
+    private int id;
+    private String description;
+    private String date;
+
+    public Task(int id, String description, String date) {
+        this.id = id;
+        this.description = description;
+        this.date = date;
+    }
+
+}
---
+++ app/src/test/java/sg/edu/rp/c346/id21030068/demodatabase/Task.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c346.id21030068.demodatabase;
+
+public class Task {
+}
